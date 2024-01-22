57674dd522af8a6653a40e4319e55732a1eb0391
Will Barnett <will.barnett@kcl.ac.uk>
2024-01-22 13:05:41 +0000

    Change to  the Xtext tool editorserver  to remove the xtext config file webserver app deploy directory (ROOT) so that it redeployed by tomcat on startup. Set min and max find depth so the deploy location is not searched recursively and the containing directory is excluded.

--- xtext/start.sh
+++ xtext/start.sh
@@ -7,8 +7,8 @@ echo Old builds cleaned.
 rm -rf ${ES_UPLOAD_LOCATION}/*
 echo Old uploads cleaned.
 
-find ${ES_DEPLOY_FILE_LOCATION}/ -type f -not -name 'ROOT.war' -delete
-find ${ES_DEPLOY_FILE_LOCATION}/ -mindepth 1 -type d -not -name 'ROOT' -exec rm -rf {} \+ 
+find ${ES_DEPLOY_FILE_LOCATION}/ -mindepth 1 -maxdepth 1 -type f -not -name 'ROOT.war' -delete
+find ${ES_DEPLOY_FILE_LOCATION}/ -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} \+ 
 echo Old editor instances cleaned.
 
 # start tomcat
