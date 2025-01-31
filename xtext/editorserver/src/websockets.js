9a02590013a475cbc0678f3b88fac0ee8cdf07a4
Mehdi Mehtarizadeh <mehdi.mehtarizadeh@kcl.ac.uk>
2025-01-31 15:56:55 +0000

    Removed file polling.

--- xtext/editorserver/src/websockets.js
+++ xtext/editorserver/src/websockets.js
@@ -49,8 +49,54 @@ function checkEditor(data){
 
 }
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+
+function subscribe_to_build(editorID) {
+    var response = get_response();
+
+    const filePath = config.deployFileLocation + "/" + editorID;
+    const buildLogPath= config.buildFileLocation + "/" + editorID + "/" + "build.log";
+    const buildStatusPath= `${config.buildFileLocation}/${editorID}/build.res`;
+    
+    var buildLog = '';
+    var buildStatus = -1;
+
+    // watch the build log file for changes
+    fs.watch(buildLogPath, () => {
+        if (fs.existsSync(buildLogPath)) {
+            buildLog = fs.readFileSync(buildLogPath, 'utf8');
+        }
+
+        if (fs.existsSync(buildStatusPath)) {
+            buildStatus = Number( fs.readFileSync(buildStatusPath, 'utf8') );
+            if (buildStatus > 0) {
+                response.error = "Please refer to the build log.";
+            }
+        }
+
+        editorDeployed = fs.existsSync(filePath);
+    
+        response.editorReady = editorDeployed;
+        response.output = buildLog;
+        ws.send(JSON.stringify(response));
+    });
+
+    // watch the deploy location to detect when the editor is depoyed
+    fs.watch(config.deployFileLocation, (...args) =>{   
+        const filename = args[1];
+        if (filename == editorID){
+            response.editorReady = true;
+            response.output = buildLog;
+            ws.send(JSON.stringify(response));
+            ws.terminate();
+        }
+    });
+}
+
+/**
+ * get a default websocket response object
+ */
+function get_response(){
+    return {'editorReady': false, 'output': '', 'error': ''};
 }
 
 function heartbeat() {
@@ -63,29 +109,10 @@ wss.on('connection', function connection(ws) {
     ws.isAlive = true;
     ws.on('error', console.error);
     ws.on('pong', heartbeat);
-
-    ws.on('message', function message(data) {
-        var response = checkEditor(data);
-        var response_length = response.output.length;
-            ws.send(JSON.stringify(response));
-        if (response.editorReady) {
-            ws.terminate()
-        }
-        else {
-        while(!response.editorReady){
-            response = checkEditor(data);
-            if (response.output.length != response_length){
-                response_length = response.output.length;
-                ws.send(JSON.stringify(response));
-            }
-        }
-
-        ws.send(JSON.stringify(response));
-        }
-    });
-
+    ws.on('message', subscribe_to_build(data));
 });
 
+// Ping and look for broken connections, every 3 seconds 
 const interval = setInterval(function ping() {
   wss.clients.forEach(function each(ws) {
     if (ws.isAlive === false) return ws.terminate();
