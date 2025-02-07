2d37fc91e07af31d5cba4d1c8324d11a88cbcf64
Mehdi Mehtarizadeh <mehdi.mehtarizadeh@kcl.ac.uk>
2025-02-07 01:03:15 +0000

    Handle watcher close.

--- xtext/editorserver/src/websockets.js
+++ xtext/editorserver/src/websockets.js
@@ -5,27 +5,29 @@ import  fs from "fs";
 /**
  * subscribes to updates from build files of Xtext project
  * @param {*} editorID - string unique project identifier @see controllers/XtextController.js
+ * @param {*} buildPathWatcher - file watch for build path
+ * @param {*} deployPathWatcher -  file watch for deploy path
  */
-function subscribe_to_build(editorID) {
+function subscribe_to_build(editorID, buildPathWatcher, deployPathWatcher) {
     var response = get_response();
 
     const filePath = config.deployFileLocation + "/" + editorID;
     const buildPath = config.buildFileLocation + "/" + editorID;
-    const buildStatusPath= `${config.buildFileLocation}/${editorID}/build.res`;
-    
+    const buildStatusFile = 'build.res';
+    const buildLogFile = 'build.log';
+
     var buildLog = '';
     var buildStatus = -1;
 
     // watch the build log file for changes
-    fs.watch(buildPath, () => {
-        const buildLogPath = buildPath + "/" + "build.log";
+    buildPathWatcher = fs.watch(buildPath, (_, fileName) => {
 
-        if (fs.existsSync(buildLogPath)) {
-            buildLog = fs.readFileSync(buildLogPath, 'utf8');
+        if (fileName == buildLogFile){
+            buildLog = fs.readFileSync(buildPath + '/' + buildLogFile, 'utf8');
         }
 
-        if (fs.existsSync(buildStatusPath)) {
-            buildStatus = Number( fs.readFileSync(buildStatusPath, 'utf8') );
+        if (fs.existsSync(buildPath + '/' + buildStatusFile)) {
+            buildStatus = Number(fs.readFileSync(buildPath + '/' + buildStatusFile, 'utf8'));
             if (buildStatus > 0) {
                 response.error = "Please refer to the build log.";
             }
@@ -37,9 +39,8 @@ function subscribe_to_build(editorID) {
     });
 
     // watch the deploy location to detect when the editor is depoyed
-    fs.watch(config.deployFileLocation, (_, fileName) =>{   
-        const filename = args[1];
-        if (filename == editorID){
+    deployPathWatcher = fs.watch(config.deployFileLocation, (_, fileName) =>{   
+        if (fileName == editorID){
             response.editorReady = true;
             response.output = buildLog;
             this.send(JSON.stringify(response));
@@ -58,21 +59,33 @@ function get_response(){
 const wss = new WebSocketServer({ port: 8000 });
 
 wss.on('connection', function connection(ws) {
+    var buildPathWatcher = deployPathWatcher = null;
     ws.isAlive = true;
-    ws.on('error', console.error);
+    ws.on('error', () => {
+        buildPathWatcher?.close();
+        deployPathWatcher?.close();
+    });
     ws.on('pong', () => { ws.isAlive = true; });
-    ws.on('message', subscribe_to_build);
+    ws.on('message', function(data) {
+        subscribe_to_build(data, buildPathWatcher, deployPathWatcher);
+    });
+    ws.on('close', () => {
+        buildPathWatcher?.close();
+        deployPathWatcher?.close();
+    });
 });
 
 // Ping and look for broken connections, every 3 seconds 
 const interval = setInterval(function ping() {
-  wss.clients.forEach(function each(ws) {
-    if (ws.isAlive === false) return ws.terminate();
+    wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false) {
+        return ws.terminate();
+    }
     ws.isAlive = false;
     ws.ping();
-  });
+    });
 }, 3000);
 
 wss.on('close', function close() {
-  clearInterval(interval);
+    clearInterval(interval);
 });
